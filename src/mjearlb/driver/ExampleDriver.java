bc5968b532392d2b02a3a1c52e0b3f966bb9da33
administrator <milobauman@gmail.com>
2023-06-08 12:02:06 -0400

    Added example of writing to a file and reading from it

--- src/mjearlb/driver/ExampleDriver.java
+++ src/mjearlb/driver/ExampleDriver.java
@@ -2,14 +2,24 @@ package mjearlb.driver;
 
 import mjearlb.objects.TestObject;
 import mjearlb.writer.WriteToFile;
-import mjearlb.writer.ReadFromFile; 
+import static mjearlb.writer.WriteToFile.writeToFile; 
+import mjearlb.writer.ReadFromFile;
+import static mjearlb.writer.ReadFromFile.readFromFile;
 
 public class ExampleDriver {
 
-    static TestObject testObj; 
+    static TestObject testObj1; 
 
     public static void main(String[] args) {
-	testObj = new TestObject(); 
+	testObj1 = new TestObject();
+	System.out.println(testObj1);
+	writeToFile(testObj1, "test1");
+
+
+	TestObject testObj2 = readFromFile("test1", TestObject.class);
+
+	System.out.println(testObj2);
+	
     } // main
 
 } // ExampleDriver
