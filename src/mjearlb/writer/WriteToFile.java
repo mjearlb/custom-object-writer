0f7b790e05f5a898533a91b056a34dc0b03cff6f
administrator <milobauman@gmail.com>
2023-06-08 00:16:24 -0400

    Added implementation for WriteToFile. Has dependencies on GSON.

--- src/mjearlb/writer/WriteToFile.java
+++ src/mjearlb/writer/WriteToFile.java
@@ -1,6 +1,25 @@
 package mjearlb.writer;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException; 
+
 public class WriteToFile {
 
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+    public static <T> void writeToFile(T obj, String title) {
+	String pathName = "resources/" + title + ".txt";
+
+	try {
+	    FileWriter fw = new FileWriter(pathName);
+	    
+	    String json = gson.toJson(obj);
+	    fw.write(json); 
+	} catch (IOException e) {
+	    System.err.println(e);
+	    e.printStackTrace(); 
+	} // try/catch
+    } // writeToFile
 } // WriteToFile
