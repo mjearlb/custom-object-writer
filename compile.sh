1f11ee6768f65efaeece9207b6243ee97fcca3ed
administrator <milobauman@gmail.com>
2023-06-08 00:16:48 -0400

    Updated compile.sh to include dependencies on GSON

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@ rm -rf bin
 
 javac -d bin src/mjearlb/objects/TestObject.java
 
-javac -d bin -cp bin src/mjearlb/writer/WriteToFile.java
+javac -d bin -cp "bin:lib/gson-2.10.1.jar" src/mjearlb/writer/WriteToFile.java
 
-javac -d bin -cp bin src/mjearlb/writer/ReadFromFile.java
+javac -d bin -cp "bin:lib/gson-2.10.1.jar" src/mjearlb/writer/ReadFromFile.java
 
-javac -d bin -cp bin src/mjearlb/driver/ExampleDriver.java
+javac -d bin -cp "bin:lib/gson-2.10.1.jar" src/mjearlb/driver/ExampleDriver.java
