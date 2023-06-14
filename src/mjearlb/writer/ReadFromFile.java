86a793aebfaad43feb75d3fc6cc20db7d20d2c1d
administrator <milobauman@gmail.com>
2023-06-14 12:55:01 -0400

    Added FileNotFoundException and javadocs

--- src/mjearlb/writer/ReadFromFile.java
+++ src/mjearlb/writer/ReadFromFile.java
@@ -3,13 +3,22 @@ package mjearlb.writer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.FileReader;
-import java.io.IOException; 
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class ReadFromFile {
 
     private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    public static <T> T readFromFile(String title, Class<T> objType) {
+    /**
+     * Reads a .txt file with the name {@code title} from the /resources
+     * directory. 
+     *
+     * @param title the name of the .txt file being read from.
+     * @param objType the type of object being written.
+     * @throws FileNotFoundException if the file cannot be found. 
+     */
+    public static <T> T readFromFile(String title, Class<T> objType) throws FileNotFoundException {
 	String pathName = "resources/" + title + ".txt";
 	try {
 	    FileReader fr = new FileReader(pathName);
